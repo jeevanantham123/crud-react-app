1b7c4cf4d1c2cf4451f16ca65c4a4c3d500fa687
jeevanantham123 <jeewa.mad.2000@gmail.com>
2022-12-13 18:06:19 +0530

    saga changes

--- src/common/store/counter/saga.ts
+++ src/common/store/counter/saga.ts
@@ -2,12 +2,8 @@ import { put, takeEvery } from "redux-saga/effects";
 import { ACTIONS } from "../sagaActions";
 import { decrement, increment } from "./slice";
 
-<<<<<<< HEAD
 function* incCounter({ payload }: any) {
   console.log(payload);
-=======
-function* incCounter() {
->>>>>>> fcb05209eee5f27ada61a22751518c4955557430
   yield put(increment());
 }
 
--- src/common/store/saga.ts
+++ src/common/store/saga.ts
@@ -1,13 +1,7 @@
 import { all } from "redux-saga/effects";
 import { helloSaga, CounterSaga } from "./counter/saga";
-<<<<<<< HEAD
 import { watchFetchProducts } from "./products/saga";
 
 export default function* rootSaga() {
   yield all([helloSaga(), CounterSaga(), watchFetchProducts()]);
-=======
-
-export default function* rootSaga() {
-  yield all([helloSaga(), CounterSaga()]);
->>>>>>> fcb05209eee5f27ada61a22751518c4955557430
 }
